28e9c16ec69af43f980cf6ca8bb6463c901f18b7
sburke781 <sburke781@gmail.com>
2020-12-30 18:53:48 +1100

    PC virtual motion sensor (#18)
    * Initial upload of PC Virtual Motion Sensor
    
    Powershell script to detect time since last user input and update a virtual motion sensor on a Hubitat hub through the Maker API.
    
    * Initial creation of Readme.md file
    
    * Setup Readme.md for PC Virtual Motion Sensor
    
    * Minor tweaks to Readme.md
    
    * Formatting changes of readme.md

---
+++ PC_VirtualMotionSensor/ConfigFile.psm1
@@ -0,0 +1,417 @@
+<#
+.SYNOPSIS
+PowerShell commandlet loading settings from a config file into the calling
+script's command-line parameters and variables.
+
+.LINK
+https://github.com/alekdavis/ConfigFile
+#>
+
+#Requires -Version 4.0
+
+<#
+.SYNOPSIS
+Sets script parameters and variables using the values in a config file.
+
+.DESCRIPTION
+Use this function to load script settings from a config file.
+
+The function will iterate through elements of the config file and, if the value
+is set, it will use it to set the script parameter or variable with the same
+name.
+
+For the sample of the config file and the explanation of the file structure,
+see online help.
+
+.PARAMETER ConfigFilePath
+Path to the config file. If not specified, the config file is considered
+optional (if the default config file is not found, no error will be returned).
+The default config file is named after the calling script with the appended
+extension reflecting the format, e.g. for the script with the name 'MyScript.ps1',
+the default JSON config file will be 'MyScript.ps1.json' located in the same
+folder.
+
+.PARAMETER Format
+Either Json or Ini.
+
+.PARAMETER Json
+Shortcut for '-Format Json'.
+
+.PARAMETER Ini
+Shortcut for '-Format Ini'.
+
+.PARAMETER DefaultParameters
+Holds a hashtable with parameters that will not be imported. To ignore parameters
+initialized via command line pass the $PSBoundParameters variable. When passing
+a custom hashtable, you only need to specify the keys.
+
+.EXAMPLE
+Import-ConfigFile
+Checks if the default JSON config file exists, and if so, loads settings from
+the file into the script variables.
+
+.EXAMPLE
+Import-ConfigFile -Ini
+Checks if the default INI config file exists, and if so, loads settings from
+the file into the script variables.
+
+.EXAMPLE
+Import-ConfigFile -ConfigFilePath "C:\Scripts\MyScript.ps1.DEBUG.json"
+Loads settings from the specified JSON config file into the script variables.
+
+.EXAMPLE
+Import-ConfigFile -ConfigFilePath "C:\Scripts\MyScript.ps1.DEBUG.ini"
+Loads settings from the specified INI config file into the script variables.
+
+.EXAMPLE
+Import-ConfigFile -DefaultParameters $PSBoundParameters
+Loads settings from the default config file into the script variables
+ignoring parameters explicitly passed via command line.
+
+.LINK
+https://github.com/alekdavis/ConfigFile
+#>
+
+function Import-ConfigFile {
+    [CmdletBinding(DefaultParameterSetName="Default")]
+    param (
+        [string]
+        $ConfigFilePath = $null,
+
+        [Parameter(ParameterSetName="Format")]
+        [ValidateSet("", "Json", "Ini")]
+        [string]
+        $Format = "",
+
+        [Parameter(ParameterSetName="Json")]
+        [switch]
+        $Json,
+
+        [Parameter(ParameterSetName="Ini")]
+        [switch]
+        $Ini,
+
+        [Hashtable]
+        $DefaultParameters = $null
+    )
+
+    # Allow module to inherit '-Verbose' flag.
+    if (($PSCmdlet) -and (-not $PSBoundParameters.ContainsKey('Verbose'))) {
+        $VerbosePreference = $PSCmdlet.GetVariableValue('VerbosePreference')
+    }
+
+    # Allow module to inherit '-Debug' flag.
+    if (($PSCmdlet) -and (-not $PSBoundParameters.ContainsKey('Debug'))) {
+        $DebugPreference = $PSCmdlet.GetVariableValue('DebugPreference')
+    }
+
+    # If config file was explicitly specified, make sure it exists.
+    if ($ConfigFilePath) {
+        if (!(Test-Path -Path $ConfigFilePath -PathType Leaf)) {
+            throw "Config file '" + $ConfigFilePath + "' is not found."
+        }
+
+        # If format is not specified, map it to the file extension.
+        if (!$Format -and !$Json -and !$Ini) {
+            $ext = [System.IO.Path]::GetExtension($ConfigFilePath)
+
+            if ($ext) {
+                $Format = $ext.Replace(".", "")
+            }
+        }
+    }
+    # If path is not specified, use the default (script + .json extension).
+    else {
+        # Default config file is named after running script with .json extension.
+
+        # First try the invoking script.
+        $ConfigFilePath = $null
+
+        # If the invoking script is a module, check the caller.
+        if ($PSCmdlet) {
+            $ConfigFilePath = $MyInvocation.PSCommandPath
+        }
+        else {
+            $ConfigFilePath = $PSCommandPath
+        }
+
+        # Set appropriate format.
+        if (!$Format) {
+            if ($Ini) {
+                $Format = "Ini"
+            }
+            else {
+                $Format = "Json"
+            }
+        }
+
+        $ext = ".$Format".ToLower()
+
+        $ConfigFilePath += $ext
+
+        # Default config file is optional.
+        if (!(Test-Path -Path $ConfigFilePath -PathType Leaf)) {
+
+            Write-Verbose "Config file '$ConfigFilePath' is not found."
+            return
+        }
+    }
+
+    $count = 0
+    Write-Verbose "Loading config file '$ConfigFilePath'."
+
+    # Process JSON file.
+    if ($Format -eq "Json") {
+        $jsonString = Get-Content $ConfigFilePath -Raw `
+            -ErrorAction:SilentlyContinue -WarningAction:SilentlyContinue
+
+        if (!$jsonString) {
+            Write-Verbose "Config file is empty."
+            return
+        }
+
+        Write-Verbose "Converting config file settings into a JSON object."
+        $jsonObject = $jsonString | ConvertFrom-Json `
+            -ErrorAction:SilentlyContinue -WarningAction:SilentlyContinue
+
+        if (!$jsonObject) {
+            Write-Verbose "Cannot convert config file settings into a JSON object."
+            return
+        }
+
+        $strict = ($jsonObject._meta.strict -or $jsonObject.meta.strict)
+        $prefix = $jsonObject._meta.prefix
+        if (!$prefix) {
+            $jsonObject.meta.prefix
+        }
+        if (!$prefix) {
+            $prefix = "_"
+        }
+
+        # Process elements twice: first, literals, then the ones that require expansion.
+        # Technically, when using the module, one pass would suffice, since the only
+        # supported expandable values are environment variables (%%) or PowerShell
+        # environment variables ($env:) and these can be resolved in a single pass.
+        # But in case this function is copied into and used directly from a script
+        # (not from a module), the second pass is needed in case a value references
+        # a script variable. Again, keep in mind that script variable expansion is not
+        # supported when using the module.
+        for ($i=0; $i -lt 2; $i++) {
+            $jsonObject.PSObject.Properties | ForEach-Object {
+
+                # Copy properties to variables for readability.
+                $hasValue   = $_.Value.hasValue
+                $name       = $_.Name
+                $value      = $null
+                $value      = $_.Value.value
+
+                # In ForEach-Object loops 'return' acts as 'continue' in  loops.
+                if ($name.StartsWith($prefix)) {
+                    # Skip to next (yes, 'return' is the right statement here).
+                    return
+                }
+
+                # If 'hasValue' is explicitly set to 'false', ignore element.
+                if ($hasValue -eq $false) {
+                    return
+                }
+
+                # Now, the 'hasValue' is either missing or is set to 'true'.
+
+                # In the strict mode, 'hasValue' must be set to include the element.
+                if ($strict -and ($null -eq $hasValue)) {
+                    return
+                }
+
+                # If 'hasValue' is not set and the value resolves to 'false', ignore it.
+                if (($null -eq $hasValue) -and (!$value)) {
+                    return
+                }
+
+                # Check if parameter is specified on command line.
+                if ($DefaultParameters) {
+                    if ($DefaultParameters.ContainsKey($name)) {
+                        return
+                    }
+                }
+
+                # Okay, we must use the value.
+
+                # The value must be expanded if it:
+                # - is not marked as a literal,
+                # - has either '%' or '$' character (not the $ end-of-line special character),
+                # - is neither of PowerShell constants that has a '$' character in name
+                #   ($true, $false, $null).
+                if ((!$_.Value.literal) -and
+                    (($value -match "%") -or ($value -match "\$")) -and
+                    ($value -ne $true) -and
+                    ($value -ne $false) -and
+                    ($null -ne $value)) {
+
+                    # Skip on the first iteration in case it depends on the unread variable.
+                    if ($i -eq 0) {
+                        $name = $null
+                    }
+                    # Process on second iteration.
+                    else {
+                        if ($value -match "%") {
+
+                            # Expand environment variable.
+                            $value = [System.Environment]::ExpandEnvironmentVariables($value)
+                        }
+                        else {
+                            # Expand PowerShell variable.
+                            $value = $ExecutionContext.InvokeCommand.ExpandString($value)
+                        }
+                    }
+                }
+                else {
+                    # Non-expandable variables have already been processed in the first iteration.
+                    if ($i -eq 1) {
+                        $name = $null
+                    }
+                }
+
+                if ($name) {
+                    if ($count -eq 0) {
+                        Write-Verbose "Setting variable(s):"
+                    }
+
+                    Write-Verbose "-$name '$value'"
+
+                    if ($PSCmdlet) {
+                        $PSCmdlet.SessionState.PSVariable.Set($name, $value)
+                    }
+                    else {
+                        Set-Variable -Scope Script -Name $name -Value $value -Force -Visibility Private
+                    }
+
+                    $count++
+                }
+            }
+        }
+    }
+    # Process INI file.
+    else {
+        # Hash table to hold contents of the INI file.
+        $iniData = @{}
+
+        # Process all lines in the INI file and save the contents in a hash.
+        switch -regex -file $ConfigFilePath
+        {
+            # If the line starts with a non-alphanumeric character...
+            "^\s*[^a-zA-Z\d]" {
+                # ...ignore it (it must be a comment or a section)
+            }
+            # Process name=value pair. The format is like:
+            #   name=value
+            #   name~=%value% (~ indicates that the string is literal and should not be expanded)
+            #   name`=$value (` indicates that the string is literal and should not be expanded)
+            #   name;=value1;value2;value3 (; is used as a delimiter for array elements)
+            #   name@@@=value1@@@value2@@@value3 (@@@ is used as a delimiter for array elements)
+            # and so on.
+            # A special character (or a string of special characters) before the equal sign
+            # can be used as an indicator that the value should not be expanded (either ` or ~
+            # can be used for this) or contain the delimiter for array elements. Notice that
+            # white spaces will not be trimmed from the value(s).
+            "^\s*([a-zA-Z\d]+)\s*([^\sa-zA-Z\d=]*)=(.*)" {
+                $name,$delimiter,$value = $matches[1..3]
+
+                # Check if parameter is specified on command line.
+                if ($DefaultParameters) {
+                    if ($DefaultParameters.ContainsKey($name)) {
+                        continue
+                    }
+                }
+
+                $iniData[$name] = $value, $delimiter
+            }
+        }
+
+        # Process in two steps in case we need to expand environment variables.
+        for ($i=0; $i -lt 2; $i++) {
+            foreach ($name in $iniData.Keys) {
+                $value     = $iniData[$name][0]
+                $delimiter = $iniData[$name][1]
+
+                # Expandable string contains % or $, does not hold the built-in
+                # PowerShell boolean or null values, and does not have a delimiter
+                # indicator characters (` or ~) in front of the equal sign.
+                if ((($value -match "%") -or ($value -match "\$")) -and
+                    ($value -ne $true) -and
+                    ($value -ne $false) -and
+                    ($null -ne $value) -and
+                    ($delimiter -notin '`', '~')) {
+
+                    # Skip on the first iteration in case it depends on the unread variable.
+                    if ($i -eq 0) {
+                        $name = $null
+                    }
+                    # Process on second iteration.
+                    else {
+                        if ($value -match "%") {
+                            # Expand environment variable.
+                            $value = [System.Environment]::ExpandEnvironmentVariables($value)
+                        }
+                        else {
+                            # Expand PowerShell variable.
+                            $value = $ExecutionContext.InvokeCommand.ExpandString($value)
+                        }
+                    }
+                }
+                else {
+                    # Non-expandable variables have already been processed in the first iteration.
+                    if ($i -eq 1) {
+                        $name = $null
+                    }
+                }
+
+                if ($name) {
+                    $var = $null
+
+                    if ($PSCmdlet) {
+                        $var =  $PSCmdlet.SessionState.PSVariable.Get($name)
+                    }
+                    else {
+                        #$var =  Get-Variable -Scope Script -Name $name -Visibility Public
+                        $var =  Get-Variable -Scope Script -Name $name
+                    }
+
+                    if ($var) {
+                        if ($count -eq 0) {
+                            Write-Verbose "Setting variable(s):"
+                        }
+
+                        Write-Verbose "-$name '$value'"
+
+                        # Process an array variable.
+                        if ($var.Value -is [Array]) {
+                            # If there is no delimiter specified, use comma.
+                            if (!$delimiter) {
+                                $delimiter = ','
+                            }
+                            $var.Value = $value -split $delimiter
+                        }
+                        # Process a boolean or a switch variable.
+                        elseif (($var.Value -is [Boolean]) -or ($var.Value -is [Switch])) {
+                            $var.Value = $value -notin 'false', '$false', '0', ''
+                        }
+                        # Process any other data type.
+                        else {
+                            $var.Value = [System.Management.Automation.LanguagePrimitives]::ConvertTo($value, $var.Value.GetType())
+                        }
+
+                        # Count the number of processed variables.
+                        $count++
+                    }
+                }
+            }
+        }
+    }
+
+    if ($count -gt 0) {
+        Write-Verbose "Done setting $count variable(s) from the config file."
+    }
+}
+
+Export-ModuleMember -Function *
---
+++ PC_VirtualMotionSensor/Readme.md
@@ -0,0 +1,50 @@
+# PC Virtual Motion Sensor
+
+## Description
+
+When working at a desk in a room with Motion Lighting, the traditional motion sensors are not always going to be triggered.  The aim of this script is to capture activity on a PC as a way to keep motion lighting rules active, i.e. lights remaining turned on, while someone is working on the PC.
+
+## How Does it Work
+The PC Virtual Motion Sensor is essentially a Windows PowerShell script that can be configured to run periodically on the PC being monitored, detecting the number of seconds since the last user input was recorded, e.g. mouse movement or keyboard strokes.  This reading is compared to a threshold resulting in a status of active or inactive being recorded in a Virtual Motion Sensor device made available through the Maker API.
+
+Once the Virtual Motion Sensor device is being updated on a regular basis, it can be used like any other motion sensor, incluiding in the built-in Motion Lighting app to keep a motion lighting rule active, or in a Rule Machine rule.
+
+## Installation and Configuration
+
+1. Hubitat Virtual Motion Sensor
+   1. Open the Devices page for your Hubitat Hub and click Add Virtual Device
+   2. Enter a Device Name
+   3. Select the Virtual Motion Sensor driver in the Type drop-down list
+   4. Optional - If you have multiple hubs and use Hub Mesh, you may want to turn on the Hub Mesh Enabled option
+   5. Click Save Device to create the new motion sensor
+   6. On the device setup page that is then displayed, note down the device id which can be obtained from the URL of the page
+   
+2. Maker API
+   1. Add the new Virtual Motion Sensor to an existing Maker API install or setup a new Maker API install
+   2. From the main page for the Maker API app, copy an example command URL 
+   
+3. PowerShell Script
+   1. Download the 6 files from this repository to the PC to be monitored:
+      * PC_VirtualMotionSensor.log
+      * PC_VirtualMotionSensor.ps1.ini
+      * PC_VirtualMotionSensor.vbs
+      * PC_VirtualMotionSensor.ps1
+      * ConfigFile.psm1
+      * ConfigFile.psd1
+   2. Open the **PC_VirtualMotionSensor.ps1.ini** file in Notepad or similar application
+   3. Set the **heIPAddress**, **heMakerAPIAppNo** and **heMakerAPIToken** settings using the example commmand URL in step 2.2
+   4. Set the **heMotionSensorDeviceId** from step 1.6
+   5. The **inactiveSecsThreshold** can be left at 5 minutes (300 seconds) or adjusted as required.  In the example of 5 minutes, whilst ever there is user input less than 5 minutes ago, the motion sensor will report as active.  Once the last user input is more than 5 minutes in the past, the motion sensor will report inactive.
+   6. The **logFile** setting is full path to the final location of the log file downloaded in step 3.1.  Tip - Hold down the shift key on the keyboard when right-clicking a file and select **Copy as path** to copy the full path to the file to the clipboard.
+   7. The **logLevel** can be set with values of INFO, DEBUG or $null to adjust how much is logged to the **PC_VirtualMotionSensor.log** file
+   
+4. Task Scheduler Task   
+   1. From the Windows Start Menu, type Task Scheduler and press Enter
+   2. Right-Click on the Task Scheduler Library and select Create Task
+   3. Give the task a Name, Description (optional) and select "Run only when user is logged on" and tick "Run with highest privileges"
+   4. Click on the Triggers tab and click the "New..." button to setup the schedule
+   5. The suggested schedule is once every minute, which requires choosing Daily, Recur every 1 days and Repeat Task Every 1 Minute (select 5 Minutes from the drop-down then change the 5 to a 1).  Also, adjust the start time at the top of the trigger settings to be whatever time each day the script should start looking for activity, e.g. 12:01:00 AM.  Click Ok to save the trigger schedule.
+   6. Click the **Actions tab** and then the **"New..."** button.  For the **Action** setting choose **Start a Program**, for the **Program / Script** paste **C:\WINDOWS\System32\wscript.exe**.  In the **Add Arguments (Optional)** paste the location of the VB Script, e.g. **"C:\HomeAutomation\Hubitat\PC_VirtualMotionSensor\PC_VirtualMotionSensor.vbs"**.  Other settings can be adjusted as required.  Click Ok to save the task.
+   7. Confirm the task runs as expected by reviewing the History for the task
+   
+5. Use the motion sensor in motion lighting or RM rules
