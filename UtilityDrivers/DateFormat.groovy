7a39dd6c4025eba95aab74cbffcc5831ef4dee85
Raul Martin <luarmr@gmail.com>
2021-02-26 17:18:56 -0800

    Update DateFormat.groovy (#31)
    * Update DateFormat.groovy (@luarmr)
    
    - Added a new htmlFriendlyDateTime attribute that can be used in dashboards, With the full time and date information, e.g. 12:17 Saturday 27 February
    - Group new Date() in one. The main reason is the incredibly unlikely possibility that the 2 Dates get a different day, month, or even year. And also saving one call to Date. :)
    - Added new Timezone Preference setting, defaulting to the local time zone on the HE hub

--- UtilityDrivers/DateFormat.groovy
+++ UtilityDrivers/DateFormat.groovy
@@ -17,43 +17,56 @@
  *    Date        Who            What
  *    ----        ---            ----
  *    2020-04-18  Simon Burke    Original Creation
- *    2021-02-10  Simon Burke	 Addition of Scheduling
- * 
+ *    2021-02-10  Simon Burke    Addition of Scheduling
+ *    2021-02-21  Raul Martin    Adding html format for dashboard
+ *    2021-02-22  Raul Martin    Adding Timezones
+ *
  */
 metadata {
 	definition (name: "Date / Time Display", namespace: "simnet", author: "Simon Burke") {
         capability "Refresh"
         attribute "formattedDate", "string"
         attribute "formattedTime", "string"
+        attribute "htmlFriendlyDateTime", "string"
 	}
 
 	preferences {
+		input(name: "timeZone", type: "enum", title: "Select TimeZone:", description: "", multiple: false, required: true, defaultValue:getLocation().timeZone.getID(), options: TimeZone.getAvailableIDs())
 		input(name: "dateFormat", type: "string", title:"Date Format", description: "Enter the date format to apply for display purposes", defaultValue: "EEEE d MMMM, yyyy", required: true, displayDuringSetup: true)
 		input(name: "timeFormat", type: "string", title:"Time Format", description: "Enter the time format to apply for display purposes", defaultValue: "HH:mm", required: false, displayDuringSetup: true)
 		input(name: "AutoUpdate", type: "bool", title:"Automatic Update", description: "Enable / Disable automatic update to date", defaultValue: true, required: true, displayDuringSetup: true)
-        	input(name: "AutoUpdateInterval", type: "ENUM", multiple: false, options: ["20", "30", "60", "300"], title:"Auto Update Interval", description: "Number of seconds between automatic updates", defaultValue: 30, required: true, displayDuringSetup: true)		
-        
+		input(name: "AutoUpdateInterval", type: "ENUM", multiple: false, options: ["20", "30", "60", "300"], title:"Auto Update Interval", description: "Number of seconds between automatic updates", defaultValue: 30, required: true, displayDuringSetup: true)
     }
 
 }
 
 def refresh() {
- runCmd()   
+ runCmd()
 }
 
 def updated() {
-
     log.debug("updated: AutoPolling = ${AutoUpdate}, StatusPollingInterval = ${AutoUpdateInterval}")
-    updatePolling()    
+    updatePolling()
 }
 
 def runCmd() {
+    now = new Date()
+    selectedTimeZone = TimeZone.getTimeZone(timeZone)
     
+    simpleDateFormatForDate = new java.text.SimpleDateFormat(dateFormat);
+    simpleDateFormatForDate.setTimeZone(selectedTimeZone);
     
-    //EEEE d MMMM, yyyy
-    //HH:mm
-    sendEvent(name: "formattedDate", value : new Date().format("${dateFormat}"));
-    sendEvent(name: "formattedTime", value : new Date().format("${timeFormat}"));
+    simpleDateFormatForTime = new java.text.SimpleDateFormat(timeFormat);
+    simpleDateFormatForTime.setTimeZone(selectedTimeZone);
+
+
+    proposedFormattedDate = simpleDateFormatForDate.format(now);
+    proposedFormattedTime = simpleDateFormatForTime.format(now);
+    proposedHtmlFriendlyDateTime = "<span class=\"timeFormat\">${proposedFormattedTime}</span> <span class=\"dateFormat\">${proposedFormattedDate}</span>"
+
+    sendEvent(name: "formattedDate", value : proposedFormattedDate);
+    sendEvent(name: "formattedTime", value : proposedFormattedTime);
+    sendEvent(name: "htmlFriendlyDateTime", value : proposedHtmlFriendlyDateTime);
 }
 
 def getSchedule() { }
@@ -64,19 +77,19 @@ def updatePolling() {
    log.debug("updatePolling: Updating Automatic Polling called, about to unschedule refresh")
    unschedule("refresh")
    log.debug("updatePolling: Unscheduleing refresh complete")
-   
+
    if(AutoUpdate == true) {
-       
+
        sched = "2/${AutoUpdateInterval} * * ? * * *"
        log.debug("updatePolling: Setting up schedule with settings: schedule(\"${sched}\",refresh)")
        try{
-           
+
            schedule("${sched}","refresh")
        }
        catch(Exception e) {
            log.error("updatePolling: Error - " + e)
        }
-       
+
        log.debug("updatePolling: Scheduled refresh set")
    }
    else { log.debug("updatePolling: Automatic status polling disabled")  }
