0bfbf17d3f1d28fd087587e48aa88c0cbcfc6782
sburke781 <sburke781@gmail.com>
2021-08-29 09:12:31 +1000

    Added batteryStatus to VirtualBatterySensor driver (#51)

--- UtilityDrivers/VirtualBatterySensor/VirtualBatterySensor_Driver.groovy
+++ UtilityDrivers/VirtualBatterySensor/VirtualBatterySensor_Driver.groovy
@@ -17,51 +17,60 @@
  *    Date        Who            What
  *    ----        ---            ----
  *    2021-08-28  Simon Burke    Original Creation
- *
+ *    2021-08-29  Simon Burke    Added batteryStatus attribute
  */
 metadata {
-	definition (name: "Virtual Battery Sensor", namespace: "simnet", author: "Simon Burke") {
-        capability "Battery"
-        attribute "battery", "number"
-        attribute "lastUpdated", "date"
-        
-        command "setBattery", [[name:"batteryReading", type: "NUMBER", description: "Enter the new battery reading (%)" ] ]
+	definition (name: 'Virtual Battery Sensor', namespace: 'simnet', author: 'Simon Burke') {
+        capability 'Battery'
+        attribute 'battery', 'number'
+        attribute 'lastUpdated', 'date'
+        attribute 'batteryStatus', 'string'
+
+        command 'setBattery', [[name:'batteryReading', type: 'NUMBER', description: 'Enter the new battery reading (%)' ] ]
 	}
 }
 
-def installed() { initialized() }
+void installed() { initialized() }
 
-def initialized() {
-    
- state.warningCount = 0;
+void initialized() {
+
+ state.warningCount = 0
+ //Detect whether battery status exists, if not, set to Idle
+  //sendEvent(name: "batteryStatus", value: "Idle");
 }
 
+void setBattery(Number pbattery) {
+    String vbatteryStatus = getBatteryStatus()
 
-
-def setBattery(Number pBattery) {
     //Check that the reading is within the 0 - 100 range for a percentage value
-    if(pBattery >= 0 && pBattery <= 100) {
-        //Update the battery attribute value
-        sendEvent(name: "battery", value: pBattery);
-        
+    if(pbattery >= 0 && pbattery <= 100) {
+        if (getBattery() == null || getBattery() == pbattery) { vbatteryStatus = 'Idle' }
+        else {
+            if (getBattery() > pbattery) { vbatteryStatus = 'Discharging' }
+            else { vbatteryStatus = 'Charging' }
+        }
+        //Update the battery and batteryStatus attributes
+        sendEvent(name: 'battery',       value: pbattery)
+        sendEvent(name: 'batteryStatus', value: vbatteryStatus)
         //Update the lastUpdated attribute value
-        def lastUpdate = new Date()
-        sendEvent(name: "lastUpdated", value : lastUpdate.format("dd/MM/yyyy HH:mm"))
-        
+        Date lastUpdate = new Date()
+        sendEvent(name: 'lastUpdated', value : lastUpdate.format('dd/MM/yyyy HH:mm'))
+
         //Reset warning count if there have been previous warnings
         if (state.warningCount > 0) {
-            state.warningCount = 0;
-            log.info("setBattery: warning count reset")
+            state.warningCount = 0
+            log.info('setBattery: warning count reset')
         }
     }
     // If the batter reading is outside the 0 - 100 range, log a warning and leave the current reading in place
     //   use the warning count state variable to make sure we don't spam the logs with repeated warnings
     else {
         if (state.warningCount < 10) {
-            state.warningCount = state.warningCount + 1;
+            state.warningCount = state.warningCount + 1
             log.warn("setBattery: Warning (${state.warningCount}) - battery level outside of 0-100 range, device not updated.  Battery value provided = ${pBattery}")
         }
     }
 }
 
-def getBattery() { return device.currentValue("battery"); }
+Number getBattery()       { return device.currentValue('battery') }
+String getBatteryStatus() { return device.currentValue('batteryStatus') }
