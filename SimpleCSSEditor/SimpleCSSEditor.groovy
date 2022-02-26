1ecc4f8126100ba0e29bed001a0cb283a52e8eb0
Simon Burke <sburke781@gmail.com>
2022-02-26 15:31:40 +1100

    Finalising Simple CSS Editor release v1

--- SimpleCSSEditor/SimpleCSSEditor.groovy
+++ SimpleCSSEditor/SimpleCSSEditor.groovy
@@ -3,13 +3,15 @@
  *
  * Used to present the Simple CSS Editor iFrame on HE Dashboards
  *
+ * Relies heavily on the great work by @thebearmay for file manager access
+ *  and @mbarone for displaying a modal iFrame
  */
 metadata {
     definition(name: 'Simple CSS Editor', namespace: 'simnet', author: 'sburke781') {
 	    capability 'Actuator'
 
-		attribute 'iFrame', 'string'
-        
+		attribute 'iFrame', 'text'
+        attribute "iFrameLauncher", "text"
         command 'refresh'
     }
 }
@@ -24,7 +26,7 @@ preferences {
 
 // Standard device methods
 void installed() {
-    debugLog('installed: BoM Radar Images device installed')
+    debugLog('installed: Simple CSS Editor device installed')
     downloadEditorHTML();
 }
 
@@ -40,33 +42,31 @@ void refresh() {
 
 void downloadEditorHTML() {
  
-    String uri = 'https://raw.githubusercontent.com/sburke781/hubitat/SimpleCSSEditor/SimpleCSSEditor/SimpleCSSEditor.html'
+    String uri = 'https://raw.githubusercontent.com/sburke781/hubitat/SimpleCSSEditor/SimpleCSSEditor.html'
 
-    def params = [
-        uri: uri,
-        contentType: 'text/plain; charset=UTF-8',
-        textParser: true,
-        headers: [:]
-    ]
-
-    try {
-        httpGet(params) { resp ->
-            if (resp != null) {
-               String data = resp.data.text;
-               writeFile('SimpleCSSEditor.html', data);
-            }
-            else {
-                errorLog('Null Response')
-            }
-        }
-    } catch (exception) {
-        errorLog("Read Error: ${exception.message}")
-        return null
-    }
+    xferFile(uri, 'SimpleCSSEditor.html');
+    setIframe();
 
 }
 
+
+def setIframe() {
+    
+    String src = "http://${location.hub.localIP}/local/SimpleCSSEditor.html";
+    
+    def launcher = "<button style='height:100%;width:100%;' onclick='document.getElementById(`${device.displayName.replaceAll('\\s','')}`).style.display=`block`;'>Simple CSS Editor</button><div id=${device.displayName.replaceAll('\\s','')} class='modal' style='display:none;position:fixed;top:0;left:10%;width:800px;height:100%;background-color:rgba(120,120,120,.85); z-index:990 !important;'><button onclick=document.getElementById('${device.displayName.replaceAll('\\s','')}').style.display='none'; style='float:right;margin:5px;'> X </button><button onclick=document.getElementById('${device.displayName.replaceAll('\\s','')}-iframe').src='${src}'; style='float:right;margin:5px;'>Refresh</button><iframe id='${device.displayName.replaceAll('\\s','')}-iframe' src='${src}' style='height:100%;width:100%;border:none;'></iframe></div>";
+    sendEvent(name: "iFrameLauncher", value: launcher)
+
+}
+
+
+
+
+
 Boolean writeFile(String fName, String fData) {
+    now = new Date()
+    String encodedString = "thebearmay$now".bytes.encodeBase64().toString();    
+
 try {
 		def params = [
 			uri: 'http://127.0.0.1:8080',
@@ -75,19 +75,19 @@ try {
 				'folder': '/'
 			],
 			headers: [
-				'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryDtoO2QfPwfhTjOuS'
+				'Content-Type': "multipart/form-data; boundary=$encodedString"
 			],
-			body: """------WebKitFormBoundaryDtoO2QfPwfhTjOuS
+            body: """--${encodedString}
 Content-Disposition: form-data; name="uploadFile"; filename="${fName}"
 Content-Type: text/plain
 
 ${fData}
 
-------WebKitFormBoundaryDtoO2QfPwfhTjOuS
+--${encodedString}
 Content-Disposition: form-data; name="folder"
 
 
-------WebKitFormBoundaryDtoO2QfPwfhTjOuS--""",
+--${encodedString}--""",
 			timeout: 300,
 			ignoreSSLIssues: true
 		]
@@ -96,9 +96,45 @@ Content-Disposition: form-data; name="folder"
 		return true
 	}
 	catch (e) {
-		errorLog "Error writing file $fName: ${e}"
+		errorLog("Error writing file $fName: ${e}")
 	}
 	return false
+}
+
+Boolean xferFile(fileIn, fileOut) {
+    fileBuffer = (String) readExtFile(fileIn)
+    retStat = writeFile(fileOut, fileBuffer)
+    return retStat
+}
+
+String readExtFile(fName){
+    def params = [
+        uri: fName,
+        contentType: "text/html",
+        textParser: true
+    ]
+
+    try {
+        httpGet(params) { resp ->
+            if(resp!= null) {
+               int i = 0
+               String delim = ""
+               i = resp.data.read() 
+               while (i != -1){
+                   char c = (char) i
+                   delim+=c
+                   i = resp.data.read() 
+               } 
+               return delim
+            }
+            else {
+                errorLog("Null Response")
+            }
+        }
+    } catch (exception) {
+        errorLog("Read Ext Error: ${exception.message}")
+        return null;
+    }
 }
 
 //Logging methods
